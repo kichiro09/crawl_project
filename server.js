d48dbbf22941d5eb40cc3f9710c1db52b1ccb4ae
kichiro09 <lehieu12d@gmail.com>
2020-12-10 10:01:19 +0700

    edit server listen port

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ app.get('/get_chapter_images', function(req, res) {
 	}
 });
 
-var server = app.listen(3000, function() {
+var server = app.listen(process.env.PORT || 3000, function() {
 	var host = server.address().address
 	var port = server.address().port
 	console.log("Ung dung Node.js dang hoat dong tai dia chi http://%s:%s",host,port)
